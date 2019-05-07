5ecde1bda68354618e848f108fe23589a0265625
YonatanOfek <38861891+YonatanOfek@users.noreply.github.com>
2019-05-07 18:41:27 +0300

    Add Data and make_pickle_file.py from Moodle

---
+++ make_pickle_file.py
@@ -0,0 +1,15 @@
+import pickle
+
+
+def make_pickle(dict, filename):
+    """
+    make pickle file
+    :param dict: the dictionary
+    :param filename: 'trained_dict'
+    """
+    with open(filename + '.pickle', 'wb') as handle:
+        pickle.dump(dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+
+
